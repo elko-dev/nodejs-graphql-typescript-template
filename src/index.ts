329ffda803c7d928ff02cfc722da34e22c3c5e90
spawn-dev <spawn-dev>
2020-03-11 17:12:26 -0600

    revert to simple postgres implementation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { GraphQLServer } from 'graphql-yoga';
-import { prisma } from './generated/prisma-client';
 import resolvers from './resolvers';
+import { createConnection } from 'typeorm';
+
+// Create Database Connection
+createConnection();
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: './src/graphql/schema.graphql',
   resolvers,
   context: request => ({
     ...request,
-    prisma,
   }),
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
