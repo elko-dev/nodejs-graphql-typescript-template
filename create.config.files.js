8bb3a2c5be9eee09c3555c7281e689ea3367ad3e
larse514 <andrew.larse514@gmail.com>
2020-03-20 16:12:30 -0500

    fix config name

--- create.config.files.js
+++ create.config.files.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const rp = require('request-promise');
 const { google } = require('googleapis');
 const SCOPES = ['https://www.googleapis.com/auth/cloud-platform'];
-const WEB_CONFIG_FILE = "./src/config/web-services.json";
+const WEB_CONFIG_FILE = "./config/web-spawn-platform.json";
 const projectName = process.argv.slice(2)[0];
 const webAppName = process.argv.slice(2)[1];
 function getAccessToken() {
     return new Promise(function (resolve, reject) {
-        var key = require('./serviceAccountKey.json');
+        var key = require('./config/spawn-platform.json');
         var jwtClient = new google.auth.JWT(
             key.client_email,
             null,
@@ -30,7 +30,6 @@ const createWebFirebaseConfig = async (projectID, webAppID) => {
     return new Promise(async (resolve, reject) => {
         try {
             const webApp = await getWebConfig(projectID, webAppID);
-            console.log(webApp)
             fs.writeFileSync(WEB_CONFIG_FILE, JSON.stringify(webApp));
             console.log('succesfully generated Web config file')
             resolve()
