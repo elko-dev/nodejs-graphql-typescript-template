01b19e90ba7d3f636afb09f358173981c9936468
larse514 <andrew.larse514@gmail.com>
2020-03-20 16:12:30 -0500

    fix config name

--- scripts/secret_decode.sh
+++ scripts/secret_decode.sh
@@ -6,11 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$SCRIPT_DIR/.."
 CONFIG_DIR="$SCRIPT_DIR/../config"
 
-if [[ -z "${FIREBASE_CONFIG}" ]]; then
+if [[ -z "${AUTH_CONFIG}" ]]; then
   echo "No firebase config found..."
 else
   rm -rf "$CONFIG_DIR"
   mkdir "$CONFIG_DIR"
   echo "Firebase config found, decoding configuration..."
-  echo "${FIREBASE_CONFIG}" | base64 --decode >"$CONFIG_DIR/spawn-platform.json"
+  echo "${AUTH_CONFIG}" | base64 --decode >"$CONFIG_DIR/spawn-platform.json"
 fi
