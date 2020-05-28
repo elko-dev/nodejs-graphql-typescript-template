d569daa7b345163a5760454baea8feb932689984
CodyTroopDev <cody.troop@elko.dev>
2020-05-28 16:05:34 -0500

    updated message when no firebase config found

--- scripts/secret_decode.sh
+++ scripts/secret_decode.sh
@@ -7,7 +7,10 @@ PROJECT_DIR="$SCRIPT_DIR/.."
 CONFIG_DIR="$SCRIPT_DIR/../config"
 
 if [[ -z "${AUTH_CONFIG}" ]]; then
-  echo "No firebase config found..."
+  echo "$(tput setaf 1)No firebase config found..."
+  echo "$(tput setaf 6)Please download the service account from $(tput sgr0)https://console.firebase.google.com/u/0/project/myapp/settings/serviceaccounts/adminsdk"
+  echo "$(tput setaf 6)and save it in the root directory as firebase-admin-sdk.json"
+  echo "$(tput setaf 3)Please ensure that it does not get checked into version control$(tput sgr0)"
 else
   rm -rf "$CONFIG_DIR"
   mkdir "$CONFIG_DIR"
