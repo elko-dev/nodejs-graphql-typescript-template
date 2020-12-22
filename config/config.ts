156237f752082f54c3cb3c68a9a588cee4b0edfc
Charlie Porth <charlieporth1@gmail.com>
2020-12-22 08:57:20 -0600

    After review fixes for this merge request

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,4 @@
 import elkoFirebaseConfig from '../src/config/web-spawn-platform.json';
-// import firebase_serviceAccount from './spawn-platform.json';
 const firebaseServiceAccount = require('./spawn-platform.json');
 export {
     firebaseServiceAccount,
