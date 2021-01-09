7043d9aab3c1c0ef46ace336ce72840977630cfa
Charlie Porth <charlieporth1@gmail.com>
2021-01-09 12:58:03 -0600

    Remove things; fixed other items

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
 import elkoFirebaseConfig from './web-spawn-platform.json';
+
 const firebaseServiceAccount = require('../../config/spawn-platform.json');
-export {firebaseServiceAccount,elkoFirebaseConfig };
+
+export {firebaseServiceAccount, elkoFirebaseConfig};
 //DOT ENV goes here
